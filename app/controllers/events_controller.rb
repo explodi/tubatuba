7a4e6f7f0d4944cbbd8262eda53bb376859d81f5
explodi <phaser@tubatuba.net>
2019-12-05 18:20:32 -0300

    nokogiri

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,12 @@ class EventsController < ApplicationController
     def index
         @events=Event.all
     end
+    def admin
+        if !current_user
+            redirect_to "/login"
+        else
+            @events=Event.all
+
+        end
+    end
 end
