2e07ecf361ec34fe0518e7adbc429ea9e274113e
explodi <phaser@tubatuba.net>
2019-12-05 19:46:43 -0300

    events

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -30,8 +30,29 @@ class AdminController < ApplicationController
     def events_update
         @event=Event.find(params[:id])
         @event.name=params[:name]
-        puts params[:start]
-        @event.start=params[:start]
+        event_start=@event.start
+        event_end=@event.end
+        if params[:start_changed]=="true"
+            event_start=DateTime.parse(params[:start]).in_time_zone("America/Santiago")
+            event_start_offset=(event_start.utc_offset)/60/60
+            event_start=(event_start-event_start_offset.hours).utc
+
+            puts event_start
+        end
+        if params[:end_changed]=="true"
+            event_end=DateTime.parse(params[:end]).in_time_zone("America/Santiago")
+            event_end_offset=(event_end.utc_offset)/60/60
+            event_end=(event_end-event_end_offset.hours).utc
+
+            puts event_end
+        end
+
+        if event_end<event_start
+            flash[:error]="event cannot start after it ends"
+        else
+            @event.start=event_start
+            @event.end=event_end
+        end
         @event.save
         redirect_to "/admin/events/edit/#{@event.id}"
     end
