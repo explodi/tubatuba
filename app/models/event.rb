cdfad7334e74e8ab5edef659c1db24bfa992503b
explodi <phaser@tubatuba.net>
2019-12-05 23:18:24 -0300

    screen

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
     end
     def screenshot
         uuid=SecureRandom.uuid
-        command="google-chrome --headless --enable-logging --virtual-time-budget=10000 --disable-gpu --no-sandbox --screenshot=\"#{Rails.root.join('public')}/#{uuid}.png\" \"https://www.tubatuba.net/\""
+        command="google-chrome --headless --enable-logging --virtual-time-budget=10000 --disable-gpu --no-sandbox --screenshot=\"#{Rails.root.join('public')}/#{uuid}.png\" \"https://tubatuba.net/\""
         puts command
         system(command)
         self.update_attribute(:screenshot_uuid,uuid)
