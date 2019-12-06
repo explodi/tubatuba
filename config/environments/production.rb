9c3739ef77ad659eb3f7573bc2017152b044a7f8
explodi <phaser@tubatuba.net>
2019-12-05 22:30:26 -0300

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.serve_static_assets = true
+  config.assets.compile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
