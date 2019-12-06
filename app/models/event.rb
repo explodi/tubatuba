bed14121192c5df074f69549531d8502a3330baa
explodi <phaser@tubatuba.net>
2019-12-05 23:07:45 -0300

    image not found

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
     def acts
         return EventAct.where({:event_id=>self.id})
     end
+    def screenshot
+        command="google-chrome --headless --enable-logging --disable-gpu --no-sandbox --screenshot=\"#{Rails.root.join('public')}/screen.png\" \"https://www.tubatuba.net/\""
+        puts command
+        system(command)
+    end
 end
