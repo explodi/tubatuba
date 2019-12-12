899529d0008468441e31d38d29a91f47544b2020
explodi <phaser@tubatuba.net>
2019-12-12 01:55:13 -0300

    MPD_CLIENT.send_command('repeat',1)

--- config/initializers/mpd.rb
+++ config/initializers/mpd.rb
@@ -3,3 +3,4 @@ mpd_host= Rails.env.development? ? 'elemurro' : 'mpd'
 MPD_CLIENT = MPD.new mpd_host, 6600
 MPD_CLIENT.connect
 MPD_CLIENT.play if MPD_CLIENT.stopped?
+MPD_CLIENT.send_command('repeat',1)
