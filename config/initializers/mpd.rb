40d847055741bf89ca6700e699a33d486fb0c7f6
explodi <phaser@tubatuba.net>
2019-12-12 02:00:51 -0300

    mpd

--- config/initializers/mpd.rb
+++ config/initializers/mpd.rb
@@ -1,6 +1,10 @@
-# require 'ruby-mpd'
-# mpd_host= Rails.env.development? ? 'elemurro' : 'mpd'
-# MPD_CLIENT = MPD.new mpd_host, 6600
-# MPD_CLIENT.connect
-# MPD_CLIENT.play if MPD_CLIENT.stopped?
-# MPD_CLIENT.send_command('repeat',1)
+require 'ruby-mpd'
+begin
+    mpd_host= Rails.env.development? ? 'elemurro' : 'mpd'
+    MPD_CLIENT = MPD.new mpd_host, 6600
+    MPD_CLIENT.connect
+    MPD_CLIENT.play if MPD_CLIENT.stopped?
+    MPD_CLIENT.send_command('repeat',1)
+rescue => e
+    puts e.message
+end
