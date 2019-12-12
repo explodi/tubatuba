81e3ab73a6c0a4aad6fa0134d0aafff699afba42
explodi <phaser@tubatuba.net>
2019-12-12 16:40:06 -0300

    song.find_info

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,15 @@ class Song < ApplicationRecord
     def find_info
         puts self.md5
         require "id3tag"
-        mp3_file = File.open(self.file_path, "rb")
-        tag = ID3Tag.read(mp3_file)
-        if tag
-            self.update_attribute(:title,tag.title) if tag.title
-            if tag.artist
-                artist=Artist.find_or_create_by({:name=>tag.artist}) 
-                self.update_attribute(:artist_id,artist.id);
+        if File.file?(self.file_path)
+            mp3_file = File.open(self.file_path, "rb")
+            tag = ID3Tag.read(mp3_file)
+            if tag
+                self.update_attribute(:title,tag.title) if tag.title
+                if tag.artist
+                    artist=Artist.find_or_create_by({:name=>tag.artist}) 
+                    self.update_attribute(:artist_id,artist.id);
+                end
             end
         end
     end
