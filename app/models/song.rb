38f11283ad19d5bc5defc67f1c81d5c97e74e787
explodi <phaser@tubatuba.net>
2019-12-12 16:38:28 -0300

    song.find_info

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,13 @@ class Song < ApplicationRecord
         require "id3tag"
         mp3_file = File.open(self.file_path, "rb")
         tag = ID3Tag.read(mp3_file)
-        self.update_attribute(:title,tag.title);
+        if tag
+            self.update_attribute(:title,tag.title) if tag.title
+            if tag.artist
+                artist=Artist.find_or_create_by({:name=>tag.artist}) 
+                self.update_attribute(:artist_id,artist.id);
+            end
+        end
     end
     def file_path
         return Rails.root.join('radio',"#{md5}.mp3")
