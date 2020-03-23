f6ce621fae84ce1c474e8a00278269f03bf6f3aa
cristian <cristian@blade.tubatuba.net>
2020-03-23 07:40:35 -0300

    GetCamerasJob

--- app/jobs/get_cameras_job.rb
+++ app/jobs/get_cameras_job.rb
@@ -7,13 +7,17 @@ class GetCamerasJob < ApplicationJob
     url="https://api.shodan.io/shodan/host/search?key=IOtrenXXJVQEkyy7adH6ztXzxn5gQ4Cv&query=#{URI::escape('webcamxp')}"
     matches=JSON.parse(open(url).read)["matches"]
     matches.each do |match|
-      @camera=SecurityCamera.find_or_create_by({
-        :ip_str=>match["ip_str"],
-        :port=>match["port"]
-      })
-      if !@camera.uuid
-        @camera.uuid=SecureRandom.uuid 
+      if @camera.where({:ip_str=>match["ip_str"]}).count==0
+
+      
+        @camera=SecurityCamera.new({
+          :ip_str=>match["ip_str"],
+          :port=>match["port"],
+          :uuid=>SecureRandom.uuid
+        })
         @camera.save
+        puts "[new camera] #{@camera.ip_str}"
+        CameraImageJob.perform_later @camera
       end
     end
     puts "[total cameras] #{SecurityCamera.all.count}"
