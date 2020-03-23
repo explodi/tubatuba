7fabdf107438be6bbdab7d55928c570a4545cff4
explodi <phaser@tubatuba.net>
2020-03-23 01:46:47 -0300

    PingLivestreamJob

--- app/jobs/ping_livestream_job.rb
+++ app/jobs/ping_livestream_job.rb
@@ -5,17 +5,11 @@ class PingLivestreamJob < ApplicationJob
     # Do something later
     puts "[PingLivestreamJob] start"
     require 'open-uri'
-    @livestream=Livestream.where({:started=>true,:ended=>false}).first
-    if @livestream
-      begin 
-        video_playlist=open("https://video.tubatuba.net/hack-the-planet/index.m3u8").read
-        puts video_playlist.inspect
-        @livestream.update_attribute(:last_ping,DateTime.now)
-        puts @livestream.inspect
-      rescue => e
-        puts "#{e.message}"
-      end
+    @livestream=Livestream.where({:started=>true,:ended=>false}).each do |livestream|
+      puts @livestream.inspect
+      puts DateTime.now.to_i-@livestream.last_ping.to_i
     end
+   
     puts "[PingLivestreamJob] stop"
 
   end
