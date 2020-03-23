9172e6d412122845458a7d93e631fc877bcdaa6c
cristian <cristian@blade.tubatuba.net>
2020-03-23 07:34:39 -0300

    class GetCamerasJob < ApplicationJob

--- app/jobs/get_cameras_job.rb
+++ app/jobs/get_cameras_job.rb
@@ -9,9 +9,13 @@ class GetCamerasJob < ApplicationJob
     matches.each do |match|
       @camera=SecurityCamera.find_or_create_by({
         :ip_str=>match["ip_str"],
-        :uuid=>SecureRandom.uuid,
-        :port=>match["port"]})
+        :port=>match["port"]}
+      )
+      if !@camera.uuid
+        @camera.uuid=SecureRandom.uuid 
+        @camera.save
       end
+    end
     puts "[total cameras] #{SecurityCamera.all.count}"
   end
 end
