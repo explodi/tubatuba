07121f929ea77a34955ba3500908ceb9888ed02e
explodi <phaser@tubatuba.net>
2020-03-23 12:10:47 -0300

    cam

--- app/controllers/security_cameras_controller.rb
+++ app/controllers/security_cameras_controller.rb
@@ -14,58 +14,52 @@ class SecurityCamerasController < ApplicationController
                 REDIS.del(current_camera_key)
             end
         end
-        if !@camera
-            @last_seen_camera=SecurityCamera.order("last_seen DESC").first
-            if @last_seen_camera 
-                puts @last_seen_camera.inspect
-                # if @last_seen_camera.save_image
-                #     REDIS.set(current_camera_key,@last_seen_camera.id)
-                @camera=@last_seen_camera
-                # else
-                #     REDIS.del(current_camera_key)
-                # end
-                FindNewCurrentCameraJob.perform_later 
+        # if !@camera
+            # @last_seen_camera=SecurityCamera.order("last_seen DESC").first
+            # if @last_seen_camera 
+            #     puts @last_seen_camera.inspect
+            #     # if @last_seen_camera.save_image
+            #     #     REDIS.set(current_camera_key,@last_seen_camera.id)
+            #     @camera=@last_seen_camera
+            #     # else
+            #     #     REDIS.del(current_camera_key)
+            #     # end
+        #         FindNewCurrentCameraJob.perform_later 
 
-            end
-        end
-        if !@camera
-            offset = rand(SecurityCamera.count) 
-            @random_camera=SecurityCamera.offset(offset).first
-            # puts @random_camera.inspect
-            # if @random_camera.save_image
-                @camera=@random_camera
-                # REDIS.set(current_camera_key,@random_camera.id)
-            # end
-            FindNewCurrentCameraJob.perform_later 
-
-        end
-        if !REDIS.exists(camera_cycle_timer)
-            if Rails.env.production?
-                FindNewCurrentCameraJob.perform_later 
-            else
-                FindNewCurrentCameraJob.perform_now 
-            end
-        #     offset = rand(SecurityCamera.count)
+        #     # end
+        # end
+        # if !@camera
+        #     offset = rand(SecurityCamera.count) 
         #     @random_camera=SecurityCamera.offset(offset).first
-        #     if @random_camera.save_image
+        #     # puts @random_camera.inspect
+        #     # if @random_camera.save_image
         #         @camera=@random_camera
-        #         REDIS.set(current_camera_key,@random_camera.id)
-        #     end
-        #     REDIS.set(camera_cycle_timer,"1")
-        #     REDIS.expire(camera_cycle_timer,10)
-        end
-        if @camera
-            if !@camera.last_camera_image
-                puts "[find new camera]"
+        #         # REDIS.set(current_camera_key,@random_camera.id)
+        #     # end
+        #     FindNewCurrentCameraJob.perform_later 
+
+        # end
+        
+        if @camera&&@camera.last_camera_image
+            # if !@camera.last_camera_image
+            #     puts "[find new camera]"
+            #     if Rails.env.production?
+            #         FindNewCurrentCameraJob.perform_later 
+            #     else
+            #         FindNewCurrentCameraJob.perform_now 
+            #     end
+            # end
+            if !REDIS.exists(camera_cycle_timer)
                 if Rails.env.production?
                     FindNewCurrentCameraJob.perform_later 
                 else
                     FindNewCurrentCameraJob.perform_now 
                 end
+    
             end
             if !REDIS.exists("screenshot:timer")
                 REDIS.set("screenshot:timer","1")
-                REDIS.expire("screenshot:timer",60)
+                REDIS.expire("screenshot:timer",2)
                 if Rails.env.production?
                     CameraImageJob.perform_later @camera
                 else
@@ -74,6 +68,7 @@ class SecurityCamerasController < ApplicationController
             end
             redirect_to @camera.last_camera_image_url
         else
+            FindNewCurrentCameraJob.perform_later 
             render :plain => "WAIT"
 
         end
