8ab5117a2cd78502cfd83fc663583e652db8fc34
cristian <cristian@blade.tubatuba.net>
2020-03-23 07:31:53 -0300

    matches

--- app/jobs/get_cameras_job.rb
+++ app/jobs/get_cameras_job.rb
@@ -5,9 +5,7 @@ class GetCamerasJob < ApplicationJob
     # Do something 
     require 'open-uri'
     url="https://api.shodan.io/shodan/host/search?key=IOtrenXXJVQEkyy7adH6ztXzxn5gQ4Cv&query=#{URI::escape('webcamxp')}"
-    puts url
     matches=JSON.parse(open(url).read)["matches"]
-    puts matches.inspect
     matches.each do |match|
       @camera=SecurityCamera.find_or_create_by({
         :ip_str=>match["ip_str"],
