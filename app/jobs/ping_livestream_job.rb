70386dc2ab91ac28a47891fe633604b128bab965
explodi <phaser@tubatuba.net>
2020-03-23 01:47:39 -0300

    PingLivestreamJob

--- app/jobs/ping_livestream_job.rb
+++ app/jobs/ping_livestream_job.rb
@@ -6,8 +6,8 @@ class PingLivestreamJob < ApplicationJob
     puts "[PingLivestreamJob] start"
     require 'open-uri'
     @livestream=Livestream.where({:started=>true,:ended=>false}).each do |livestream|
-      puts @livestream.inspect
-      puts DateTime.now.to_i-@livestream.last_ping.to_i
+      puts livestream.inspect
+      puts DateTime.now.to_i-livestream.last_ping.to_i
     end
    
     puts "[PingLivestreamJob] stop"
